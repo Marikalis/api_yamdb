d0de1fa67c39088516c67a50877dfb1ab11a73c8
Мария Лисицкая <lisitskayamari@gmail.com>
2021-12-16 18:41:40 +0000

    added get token

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,13 +4,6 @@ from rest_framework.validators import UniqueValidator
 from reviews.models import User
 
 
-# class UserSerializer(serializers.ModelSerializer):
-#     # user = SlugRelatedField(slug_field='username', read_only=True)
-
-#     class Meta:
-#         fields = ('username', 'email')
-#         model = User
-
 class UserSerializer(serializers.Serializer):
     username = serializers.CharField()
     email = serializers.EmailField(validators=[UniqueValidator(queryset=User.objects.all())])
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -44,36 +44,3 @@ class User(AbstractUser):
 
     class Meta(AbstractUser.Meta):
         swappable = 'AUTH_USER_MODEL'
-
-
-class AnonymousUser(User):
-    id = None
-    pk = None
-    username = ''
-    is_staff = False
-    is_active = False
-    is_superuser = False
-
-    def __str__(self):
-        return 'AnonymousUser'
-
-    def __eq__(self, other):
-        return isinstance(other, self.__class__)
-
-    def __hash__(self):
-        return 1  # instances always return the same hash value
-
-    def __int__(self):
-        raise TypeError()
-
-    def save(self):
-        raise NotImplementedError()
-
-    def delete(self):
-        raise NotImplementedError()
-
-    def set_password(self, raw_password):
-        raise NotImplementedError()
-
-    def check_password(self, raw_password):
-        raise NotImplementedError()
